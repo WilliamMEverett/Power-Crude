45ff08487f730012c1f2493b3b6337b730841234
WilliamMEverett <williammeistereverett@yahoo.com>
2020-01-05 19:21:28 -0700

    Initial Commit

---
+++ Power Crude/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Power Crude
+//
+//  Created by William Everett on 1/5/20.
+//  Copyright © 2020 William Everett. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Power Crude/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Power Crude
+//
+//  Created by William Everett on 1/5/20.
+//  Copyright © 2020 William Everett. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
